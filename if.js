2cf1de1d24704c04a91c87e47a3e8229e48ae541
shoyonp <shoyonkumar143@gmail.com>
2024-08-29 23:00:25 +0600

    added if file

---
+++ if.js
@@ -0,0 +1,51 @@
+/**
+ * if you bye food for me i will come
+ * 
+ * -----------------------------------
+ * 
+ * if biriyanis price is less than 300 tk i will buy
+ * 
+ * -----------------------------------
+ * 
+ * -------STRUCTURE----------
+ * 
+ * if (condition){
+ * 
+ * }
+ * 
+ * ----------------------------------
+ * 
+ * if(condition){
+ * 
+ * }
+ */
+
+
+if(3 < 10){
+    // console.log('i have a smaller value');
+}
+
+var biriyaniPrice = 200;
+
+if(biriyaniPrice < 300){
+    console.log('mama give me some biriyani');
+}
+
+/**
+ * if(condition){
+ *  execute code if condition above is true
+ * }
+ * else {
+ *  execute code if condition above is flase
+ * }
+ */
+
+
+var weight = 20;
+
+if(weight < 20){
+    console.log('i will carry it by myself');
+}
+else{
+    console.log('i will rent a rickshaw');
+}
