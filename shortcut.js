58f689bec93acf043a98b9c3e7414810bc8b5962
shoyonp <shoyonkumar143@gmail.com>
2024-08-30 00:31:01 +0600

    added logical not file

---
+++ shortcut.js
@@ -0,0 +1,17 @@
+const isLeader = true;
+// if(isLeader === true)
+// if(isLeader){
+//     console.log('leader asche');
+// }
+// else{
+//     console.log('vitore jn na ashe');
+// }
+
+const isPassed = true;
+// if(isPassed === false)
+if(!isPassed){
+    console.log('tui kida');
+}
+else{
+    console.log('ok');
+}
