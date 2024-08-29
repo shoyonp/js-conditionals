96e595b544de64cff6e8682d67deea9d786e53ab
shoyonp <shoyonkumar143@gmail.com>
2024-08-29 23:51:11 +0600

    added nested file

---
+++ nested.js
@@ -0,0 +1,17 @@
+const  money = 0;
+if(money > 300){
+    console.log('borolox');
+}
+else{
+    if(money > 100){
+        console.log('not gorib not borolok')
+    }
+    else{
+        if(money > 0){
+            console.log('thanks vai')
+        }
+        else{
+            console.log('ami kida')
+        }
+    }
+}
