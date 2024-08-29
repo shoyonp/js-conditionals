75542661627cc8555efdc8943d1dac3b8503d128
shoyonp <shoyonkumar143@gmail.com>
2024-08-29 21:00:33 +0600

    added  comparison file

---
+++ comparison.js
@@ -0,0 +1,33 @@
+// console.log(5 > 10);
+// console.log(5 < 10);
+// console.log(5 == 10);
+// console.log(10 == 10);
+// console.log(15 >= 10);
+// console.log(5 >= 10);
+// console.log(5 <= 10);
+// console.log(10 <= 10);
+
+// console.logb(10 == 10);
+// console.logb(10 == 10.1);
+
+// ---------------------
+
+var peyaraPrice = 40;
+var applePrice = 250;
+
+// console.log(peyaraPrice > applePrice);
+// console.log(peyaraPrice < applePrice);
+
+
+
+/**
+ *  ==
+ *  ===
+ */
+
+// console.log(10 == '10');
+// console.log(10 === '10');
+
+// ----------------
+console.log(10 != '10');
+console.log(10 !== '10');
