c545c1cce186369085f433363a50d9c3de569828
shoyonp <shoyonkumar143@gmail.com>
2024-08-29 20:31:22 +0600

    variables

---
+++ variable.js
@@ -0,0 +1,31 @@
+//  variable ==> container
+
+// number
+var sunglassPrice = 80;
+
+// string
+var subject = "CSE";
+
+// boolean
+var passed = true;
+
+/**
+ * naming conveniton
+ * 1. single word 
+ * 2. no quote
+ * 3. no gap or dash
+ * 4. can not start with a number 
+ * 5. can not be a key word
+ * 6. prefer camelcase
+ */
+
+var lastExamResult = 90;
+
+/**
+ * operation 
+ * 1. +, -, *, /, %
+ * 2. +=, -=, *=, /=
+ * 3. isNaN ?
+ * 4. null
+ * 5. Number
+ */
